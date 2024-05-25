1e6ee06dd877fc07ceee8ea2d9c88d8e0600ce3e
nancyh13500 <nancy.hanoyan@free.fr>
2024-05-25 18:25:35 +0200

    ajout carousel et modif img

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/habitats", "Habitats", "/pages/habitats.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
