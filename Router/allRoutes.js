043b6bd4b7c503ce7f88bcc5534c7513f4133482
nancyh13500 <nancy.hanoyan@free.fr>
2024-08-11 17:10:45 +0200

    changement color bg dark

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,7 @@ export const allRoutes = [
     new Route("/habitats", "Habitats", "/pages/habitats.html"),
     new Route("/services", "Nos services", "/pages/services.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/connexion", "Contact", "/pages/connexion.html"),
+    new Route("/connexion", "Connexion/Déconnexion", "/pages/connexion.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
